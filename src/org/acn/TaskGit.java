5c4975618c77270a15421f88b940b46157f1d418
priyaantony <priyaprinceav@gmail.com>
2021-12-14 15:13:13 +0530

    priya

--- src/org/acn/TaskGit.java
+++ src/org/acn/TaskGit.java
@@ -5,11 +5,13 @@ public class TaskGit {
 	public void addMaster() {
 		System.out.println("adding to master");
 	}
-	
+	public void addPriya() {
+		System.out.println("pushing to priya");
+	}
 	public static void main(String[] args) {
 		TaskGit g = new TaskGit();
 		g.addMaster();
-	
+		g.addPriya();
 	}
 	
 }

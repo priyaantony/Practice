08d43bdd13df4ac144db7566efb211a968afb0f2
priyaantony <priyaprinceav@gmail.com>
2021-12-14 13:16:36 +0530

    adding to master done

---
+++ src/org/acn/TaskGit.java
@@ -0,0 +1,15 @@
+package org.acn;
+
+public class TaskGit {
+
+	public void addMaster() {
+		System.out.println("adding to master");
+	}
+	
+	public static void main(String[] args) {
+		TaskGit g = new TaskGit();
+		g.addMaster();
+	
+	}
+	
+}

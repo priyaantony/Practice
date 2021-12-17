83139f31437ad20e255f49dbe2077f292fc9d064
priyaantony <priyaprinceav@gmail.com>
2021-12-17 14:47:48 +0530

    push to jeni

--- src/org/acn/TaskGit.java
+++ src/org/acn/TaskGit.java
@@ -8,11 +8,19 @@ public class TaskGit {
 	public void addJeni()	{
 		System.out.println("pushing to jeni");
 	}
-	
+	public void addJeni1() {
+		System.out.println("pushing to jeni1");
+	}
 	public static void main(String[] args) {
 		TaskGit g = new TaskGit();
 		g.addMaster();
 		g.addJeni();
+		
+		
+		
+		
+		
+		
 		
 	}
 	

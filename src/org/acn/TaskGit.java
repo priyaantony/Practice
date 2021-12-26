4df2e15f15789dcdae27b35323a9cac451b8e47e
priyaantony <priyaprinceav@gmail.com>
2021-12-26 18:54:16 +0530

    merging to master

--- src/org/acn/TaskGit.java
+++ src/org/acn/TaskGit.java
@@ -16,18 +16,15 @@ public class TaskGit {
 	public void addPriya() {
 		System.out.println("pushing to priya");
 	}
+
+	public void addJeni3() {
+		System.out.println("pushing to jeni3");
+	}
 	
 	private void addPriya1() {
 		System.out.println("push to priya");
 	}
-	
-<<<<<<< HEAD
-	public void addJeni3() {
-		System.out.println("pushing to jeni3");
-	}
-=======
-	
->>>>>>> d0c31ed84dc5642d8b928261df149e646893b98c
+
 	
 	public static void main(String[] args) {
 		TaskGit g = new TaskGit();
